fa8293adbd3ff18e8a12daa0dfb7d0f6854d4bff
SubmergingSky <121769453+SubmergingSky@users.noreply.github.com>
2025-07-01 12:48:05 +0100

    Adjusted NN evaluation and added resampling

--- neural.py
+++ neural.py
@@ -1,7 +1,13 @@
 import numpy as np
-from sklearn.neural_network import MLPClassifier
 import json
 import argparse
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, f1_score
+from sklearn.inspection import PartialDependenceDisplay
+from imblearn.over_sampling import BorderlineSMOTE
+from collections import Counter
+
 
 def parser():
     parser = argparse.ArgumentParser(description="Uses ANNs to make predictions on particle classifications.")
@@ -34,9 +40,30 @@ def dataUnpack(dataFile):
 
 def classification(data):
     features, targets = data
-    clf = MLPClassifier(solver="lbfgs", alpha=1e-5, hidden_layer_sizes=(3), max_iter=500, random_state=1)
-    clf.fit(features, targets)
+    smote = BorderlineSMOTE(random_state=1)
+    print(f"Original dataset shape: {Counter(targets)}")
+    featuresResampled, targetsResampled = smote.fit_resample(features, targets)
+    print(f"Resampled dataset shape: {Counter(targetsResampled)}")
+
+    clf = MLPClassifier(solver="lbfgs", alpha=1e-5, hidden_layer_sizes=(50), max_iter=1000, random_state=1)
+    clf.fit(featuresResampled, targetsResampled)
     return clf
+
+def evaluation(clf, testingData):
+    prediction = clf.predict(testingData[0])
+    f1score = f1_score(testingData[1], prediction, average="macro")
+
+    print("--- Classification Report ---")
+    print(classification_report(testingData[1], prediction))
+    print("--- Macro-Averaged F1 Score ---")
+    print(f"Macro-Averaged F1-Score: {f1score:.3f}")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    PartialDependenceDisplay.from_estimator(clf, testingData[0], features=[0,1,2,3,4,5], ax=ax)
+    plt.show()
+
+    return None
+
 
 def main():
     args = parser()
@@ -49,10 +76,8 @@ def main():
     print("Neural network fitted")
 
     testingData = dataUnpack(testingFile)
-    print(clf.predict(testingData[0]))
-    print("Accuracy:  ", clf.score(*testingData))
+    evaluation(clf, testingData)
 
-
+    return None
     
-
 main()
